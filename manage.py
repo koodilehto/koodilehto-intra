33e9b830394fecaa304083270e04d2b03fc8025d
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-14 18:23:37 +0300

    Test coverage command. Readme update.

--- manage.py
+++ manage.py
@@ -33,13 +33,34 @@ def list_routes():
         output.append([rule.endpoint, methods, url])
     print(tabulate(output, headers="firstrow"))
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Test coverage summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+
 
 if __name__ == '__main__':
     manager.run()
