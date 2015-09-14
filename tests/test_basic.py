054293da919d90c887bcc7b1ab5f9295ba26869e
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-14 17:04:07 +0300

    Initial test config. No db init yet.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,19 +1,23 @@
-import unittest
+# http://pythonhosted.org/Flask-Testing/
 from flask import current_app
-from app import create_app  # , db
+from flask.ext.testing import TestCase
+from app import create_app as the_create_app  # , db
 
 
-class BasicsTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app('testing')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        # db.create_all()
+class BasicsTestCase(TestCase):
+    def create_app(self):
+        return the_create_app('testing')
+
+    # def setUp(self):
+    #     self.app = create_app()
+    #     self.app_context = self.app.app_context()
+    #     self.app_context.push()
+    #     # db.create_all()
 
     # def tearDown(self):
-        # db.session.remove()
-        # db.drop_all()
-        # self.app_context.pop()
+    #     # db.session.remove()
+    #     # db.drop_all()
+    #     self.app_context.pop()
 
     def test_app_exists(self):
         self.assertFalse(current_app is None)
